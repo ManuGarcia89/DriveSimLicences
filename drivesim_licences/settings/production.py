fabe6ee65e350ce3127d1aa8f23db46577707018
ManuGarcia89 <manuel.garcia@unillanos.edu.co>
2026-05-04 14:21:25 -0500

    csrf

--- drivesim_licences/settings/production.py
+++ drivesim_licences/settings/production.py
@@ -7,6 +7,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
+CSRF_TRUSTED_ORIGINS = [f"https://{host}" for host in ALLOWED_HOSTS if host]
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
