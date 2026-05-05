aca27f1f996a350718738be5135625dfad009b20
ManuGarcia89 <manuel.garcia@unillanos.edu.co>
2026-05-04 19:39:01 -0500

    docker fix

--- drivesim_licences/settings/production.py
+++ drivesim_licences/settings/production.py
@@ -1,22 +1,27 @@
 import os
 from .base import *
 
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = True
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'api.jausim.com,localhost,127.0.0.1').split(',')
 
-CSRF_TRUSTED_ORIGINS = [f"https://{host}" for host in ALLOWED_HOSTS if host] + [f"http://{host}" for host in ALLOWED_HOSTS if host]
+CSRF_TRUSTED_ORIGINS = ['https://api.jausim.com']
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+USE_X_FORWARDED_HOST = True
+USE_X_FORWARDED_PORT = True
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_DB'),
-        'USER': os.environ.get('POSTGRES_USER'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'NAME': os.environ.get('POSTGRES_DB', 'drivesim_db'),
+        'USER': os.environ.get('POSTGRES_USER', 'drivesim_user'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'drivesim_pass'),
         'HOST': os.environ.get('DB_HOST', 'db'),
         'PORT': os.environ.get('DB_PORT', '5432'),
     }
